7119c7da8ec0d73e48f3804cbef787ed0fb69231
Jake Levine <jake@agileramen.com>
2016-10-14 14:44:01 -0700

    Add more initial config

--- config/application.rb
+++ config/application.rb
@@ -8,19 +8,22 @@ Bundler.require(*Rails.groups)
 
 module Drkiq
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # We want to set up a custom logger which logs to STDOUT.
+    # Docker expects your application to log to STDOUT/STDERR and to be ran
+    # in the foreground.
+    config.log_level = :debug
+    config.log_tags  = [:subdomain, :uuid]
+    config.logger    = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    # Since we're using Redis for Sidekiq, we might as well use Redis to back
+    # our cache store. This keeps our application stateless as well.
+    config.cache_store = :redis_store, ENV['CACHE_URL'],
+                         { namespace: 'drkiq::cache' }
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    # If you've never dealt with background workers before, this is the Rails
+    # way to use them through Active Job. We just need to tell it to use Sidekiq.
+    config.active_job.queue_adapter = :sidekiq
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    # ...
   end
 end
